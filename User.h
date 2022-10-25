95b6e6fc8750133bf355774da19fb5d23475151a
Jehe6447 <114559763+Jehe6447@users.noreply.github.com>
2022-10-25 17:59:20 -0600

    Create User.h

---
+++ User.h
@@ -0,0 +1,30 @@
+class User{
+    
+    
+    
+    private:
+        static const int size_ = 50;
+        string username_;
+        int likes_[size_];
+        int num_Posts_;
+        
+        
+        
+    public:
+        User();
+
+        User(string username, int likes[], int num_posts);
+
+
+        string getUsername();
+        void setUsername(string username);
+        int getLikesAt(int post_id);
+        bool setLikesAt(int post_id, int num_likes);
+        void setNumPosts(int posts);
+        int getNumPosts();
+        int getSize();
+        
+        
+        
+
+};
