6da7ac4b933a665352119cdaff333461307609c7
Jehe6447 <114559763+Jehe6447@users.noreply.github.com>
2022-10-25 16:00:43 -0600

    Update Readdriver.cpp

--- Readdriver.cpp
+++ Readdriver.cpp
@@ -1,3 +1,4 @@
+int readLikes (string fileName, User users[],int numUsersS, int usersArrSize, int maxPosts)
 {
 
     if(numUsersS == usersArrSize)
@@ -16,12 +17,31 @@
         if(line != "")
         {
             string temp[51];
+            bool unCheck = false;
             split(line, ',', temp, 51) ;
-            int tempLikes[50];
-            numUsersS++;
             
+            for(int i=0; i< numUsersS; i++)
+            {
+                if(users[i].getUsername() == temp[0])
+                {
+                    unCheck= true;
+                }
+            }
+            if (unCheck == false)
+            {
+            User tUser = User();
+            tUser.setUsername(temp[0]);
+            tUser.setNumPosts(maxPosts);
+            
+            for(int i= 0; i < maxPosts; i++)
+            {
+                tUser.setLikesAt(i, stoi(temp[i+1]));
+            }
+            users[numUsersS] = tUser;
+            numUsersS++;
+            }
         }
         
     }
-    return numUsersS;
+return numUsersS;
 }
