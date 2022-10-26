79e9436268cc4131507c8ad58af478541bad5651
Jehe6447 <114559763+Jehe6447@users.noreply.github.com>
2022-10-25 20:44:27 -0600

    Update User.h

--- User.h
+++ User.h
@@ -1,24 +1,16 @@
 #include <string>
+
 using namespace std;
 
 class User{
-    
-    
-    
     private:
         static const int size_ = 50;
         string username_;
         int likes_[size_];
-        int num_Posts_;
-        
-        
-        
+        int num_Posts_;        
     public:
         User();
-
         User(string username, int likes[], int num_posts);
-
-
         string getUsername();
         void setUsername(string username);
         int getLikesAt(int post_id);
@@ -26,8 +18,4 @@ class User{
         void setNumPosts(int posts);
         int getNumPosts();
         int getSize();
-        
-        
-        
-
 };
